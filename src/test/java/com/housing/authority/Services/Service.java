1cf05bfbf46e30110b95bb2c32edbec13db300ab
sergedontsa <dontsaserge@gmail.com>
2020-06-29 17:30:15 -0400

    employess test done

---
+++ src/test/java/com/housing/authority/Services/Service.java
@@ -0,0 +1,9 @@
+package com.housing.authority.Services;
+
+public interface Service {
+    void readAllTest();
+    void readOneTest();
+    void createTest();
+    void updateTest();
+    void deleteTest();
+}
--- src/test/java/com/housing/authority/Tuples/ApartmentTest.java
+++ src/test/java/com/housing/authority/Tuples/ApartmentTest.java
@@ -1,26 +1,57 @@
 package com.housing.authority.Tuples;
 
 import com.housing.authority.Repository.ApartmentRepository;
+import com.housing.authority.Services.Service;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Iterator;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class ApartmentTest {
+class ApartmentTest implements Service {
+
     @Autowired
     ApartmentRepository apartmentRepository;
 
 
+    @Override
+    @Test
+    @Order(1)
+    public void readAllTest() {
 
+    }
+
+    @Override
+    @Test
+    @Order(2)
+    public void readOneTest() {
+
+    }
+
+    @Override
+    @Test
+    @Order(3)
+    public void createTest() {
+
+    }
+
+    @Override
+    @Test
+    @Order(4)
+    public void updateTest() {
+
+    }
+
+    @Override
+    @Test
+    @Order(5)
+    public void deleteTest() {
+
+    }
 }
