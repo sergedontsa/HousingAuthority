70cbbdf7ed3b6fccf48a8916383e22cc9f638df8
sergedontsa <dontsaserge@gmail.com>
2020-07-01 18:19:13 -0400

    user test implemented

--- src/test/java/com/housing/authority/Tuples/UsersTest.java
+++ src/test/java/com/housing/authority/Tuples/UsersTest.java
@@ -1,7 +1,10 @@
 package com.housing.authority.Tuples;
 
 import com.housing.authority.Repository.UserRepository;
+import com.housing.authority.Resources.Constant;
+import com.housing.authority.Resources.IDGenerator;
 import com.housing.authority.Services.Service;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -10,11 +13,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UsersTest implements Service {
+
+    private String idForTest;
+    private String idForCreate;
+    private String username;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private String role;
+    private String registerDate;
+    private String lastUpdate;
+
+    @BeforeEach
+    void init(){
+        this.idForCreate = IDGenerator.USER_ID();
+        this.idForTest = "158FLI2";
+        this.username = "Username Test";
+        this.phoneNumber = "4444444444";
+        this.email = "testemail@gamil.com";
+        this.password = "password";
+        this.role = "Role";
+        this.registerDate = Constant.getCurrentDateAsString();
+        this.lastUpdate = Constant.getCurrentDateAsString();
+
+    }
+
     @Autowired
     UserRepository userRepository;
 
@@ -22,13 +58,17 @@ class UsersTest implements Service {
     @Test
     @Order(1)
     public void readAllTest() {
-
+        List<Users> users = this.userRepository.findAll();
+        assertThat(users).size().isGreaterThanOrEqualTo(0);
     }
 
     @Override
     @Test
     @Order(2)
     public void readOneTest() {
+        Users users = this.userRepository.getOne(idForTest);
+        assertNotNull(users);
+        assertEquals(idForTest, users.getUserId());
 
     }
 
@@ -36,6 +76,17 @@ class UsersTest implements Service {
     @Test
     @Order(3)
     public void createTest() {
+        Users users = new Users();
+        users.setUserId(idForCreate);
+        users.setUsername(username);
+        users.setPhonenumber(phoneNumber);
+        users.setEmail(email);
+        users.setPassword(password);
+        users.setRole(role);
+        users.setRegisterDate(registerDate);
+        users.setLastUpdate(lastUpdate);
+        Users savedUser = this.userRepository.save(users);
+        assertNotNull(savedUser);
 
     }
 
@@ -43,13 +94,33 @@ class UsersTest implements Service {
     @Test
     @Order(4)
     public void updateTest() {
+        Users existingUser = this.userRepository.getOne(idForTest);
+        assertNotNull(existingUser);
+        assertEquals(idForTest, existingUser.getUserId());
+        existingUser.setUsername(username);
+        existingUser.setPhonenumber(phoneNumber);
+        existingUser.setEmail(email);
+        existingUser.setPassword(password);
+        existingUser.setRole(role);
+        existingUser.setRegisterDate(registerDate);
+        existingUser.setLastUpdate(lastUpdate);
 
+        Users updatedUser = this.userRepository.saveAndFlush(existingUser);
+        assertNotNull(updatedUser);
     }
 
     @Override
     @Test
     @Order(5)
     public void deleteTest() {
-
+        boolean isPresent = this.userRepository.findById(idForTest).isPresent();
+        if (isPresent){
+            assertNotNull(this.userRepository.findById(idForTest).get());
+            this.userRepository.delete(this.userRepository.findById(this.idForTest).get());
+            boolean isAbsent = this.userRepository.findById(idForTest).isPresent();
+            assertFalse(isAbsent);
+        }else {
+            fail("Could not locate the entity for test");
+        }
     }
 }
