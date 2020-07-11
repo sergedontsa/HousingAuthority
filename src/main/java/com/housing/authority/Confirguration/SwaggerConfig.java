22534f5f562190203669a1d22a91588e1662c1ac
sergedontsa <dontsaserge@gmail.com>
2020-07-11 13:34:00 -0400

    Swagger configuration done

--- src/main/java/com/housing/authority/Confirguration/SwaggerConfig.java
+++ src/main/java/com/housing/authority/Confirguration/SwaggerConfig.java
@@ -1,34 +1,56 @@
 package com.housing.authority.Confirguration;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
-
-@Primary
+@EnableSwagger2
 public class SwaggerConfig
 {
+    @Bean
+    public LinkDiscoverers discoverers(){
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
 
-//    @Bean
-//    public Docket api(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(apiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(paths())
-//                .build();
-//    }
-//    private ApiInfo apiInfo(){
-//        return new ApiInfoBuilder()
-//                .title("Housing Autority Api")
-//                .description("description")
-//                .version("1.0-SNAPSHOT")
-//                .build();
-//    }
-//    private Predicate<String> paths(){
-//        return Predicates.and(
-//                PathSelectors.regex("/.*"),
-//                Predicates.not(PathSelectors.regex("/error.*"))
-//        );
-//    }
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(paths())
+                .build();
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Housing Autority Api")
+                .description("description")
+                .version("1.0-SNAPSHOT")
+                .build();
+    }
+    private Predicate<String> paths(){
+        return Predicates.and(
+                PathSelectors.regex("/.*"),
+                Predicates.not(PathSelectors.regex("/error.*"))
+        );
+    }
 }
