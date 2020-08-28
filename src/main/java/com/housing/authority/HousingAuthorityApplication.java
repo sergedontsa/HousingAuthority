936460a11b06a27385e5a69187e48a602ba20b21
sergedontsa <63218303+sergedontsa@users.noreply.github.com>
2020-08-28 12:08:22 -0400

    bug fix

--- src/main/java/com/housing/authority/HousingAuthorityApplication.java
+++ src/main/java/com/housing/authority/HousingAuthorityApplication.java
@@ -1,29 +1,18 @@
 package com.housing.authority;
 
 
-import ch.qos.logback.classic.pattern.MessageConverter;
 import com.housing.authority.Tuples.Apartment.ApartmentDocument;
 import com.housing.authority.Tuples.Building.BuildingDocument;
 import com.housing.authority.Tuples.Employee.EmployeeDocument;
 import com.housing.authority.Tuples.Tenant.TenantDocument;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MessageType;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-import javax.jms.ConnectionFactory;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableScheduling
@@ -35,8 +24,8 @@ import javax.jms.ConnectionFactory;
         BuildingDocument.class
 })
 @ConfigurationPropertiesScan("com.housing.authority")
-@EnableJms
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+//@EnableJms
+//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class HousingAuthorityApplication {
 
 
