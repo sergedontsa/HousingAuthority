8eada5e425bbd8b1f48f043f60556a513e7ad80e
sergedontsa <63218303+sergedontsa@users.noreply.github.com>
2020-12-21 21:35:24 -0500

    building address controller done

--- src/main/java/com/housing/authority/Controllers/Building/BuildingAddressController.java
+++ src/main/java/com/housing/authority/Controllers/Building/BuildingAddressController.java
@@ -1,16 +1,18 @@
 package com.housing.authority.Controllers.Building;
 
-
 import com.housing.authority.Exception.ResourceNotFoundException;
 import com.housing.authority.Repository.Building.BuildingAddressRepository;
 import com.housing.authority.Repository.Building.BuildingRepository;
 import com.housing.authority.Resources.Constant;
 import com.housing.authority.TupleAssembler.BuildingAddressModelAssembler;
 import com.housing.authority.Tuples.Building.BuildingAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -32,6 +34,7 @@ import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.metho
 @RequestMapping(value = Constant.BUILDING_ADDRESS_CONTROLLER)
 public class BuildingAddressController {
 
+    private static final Logger logger = LoggerFactory.getLogger(BuildingAddressController.class);
     @Autowired
     private final BuildingAddressRepository buildingAddressRepository;
     @Autowired
@@ -85,13 +88,30 @@ public class BuildingAddressController {
     }
     @CrossOrigin
     @PutMapping(value = Constant.BUILDING_ADDRESS_UPDATE_WITH_BUILDING_ID, consumes = Constant.CONSUMES, produces = Constant.PRODUCE)
-    public Object update(@PathVariable int buildingId, @RequestBody BuildingAddress address) {
-        return null;
+    public ResponseEntity<?> update(@PathVariable String buildingId, @RequestBody BuildingAddress address) {
+        logger.info("[UPDATE ADDRESS] - BUILDING ID: " + buildingId);
+        if (!this.buildingRepository.existsById(buildingId)){
+            logger.error("BUILDING ID: " + buildingId + " NOT FOUND");
+            return new  ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        address.setBuildingid(buildingId);
+        this.buildingAddressRepository.save(address);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+
     }
     @CrossOrigin
     @DeleteMapping(value = Constant.BUILDING_ADDRESS_DELETE_WITH_BUILDING_ID)
-    public void delete(@PathVariable String buildingId) {
-
+    public ResponseEntity<?> delete(@PathVariable String buildingId) {
+        logger.warn("[DELETE ADDRESS] - BUILDING ID = " + buildingId);
+        if (!buildingAddressRepository.existsById(buildingId)){
+            logger.error("BUILDING ID: " + buildingId + " NOT FOUND");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        this.buildingAddressRepository.deleteById(buildingId);
+        logger.info("[DELETE ADDRESS] - SUCCESS - BUILDING ID:" + buildingId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
     private boolean isBuildingExist(String buildingId){
         if (!this.buildingRepository.existsById(buildingId)){
